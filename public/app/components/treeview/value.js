c92d8544f372142051cb717fae317e6da165f11d
vincent-tr <vincent@stoic.com>
2018-07-08 16:35:43 +0200

    collapse behavior

--- public/app/components/treeview/value.js
+++ public/app/components/treeview/value.js
@@ -9,6 +9,58 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Button     from '@material-ui/core/Button';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess     from '@material-ui/icons/ExpandLess';
+import ExpandMore     from '@material-ui/icons/ExpandMore';
+
+class ExtensibleValue extends React.Component {
+  state = {
+    isSubValue: false,
+    open: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if(props.isSubValue !== state.isSubValue) {
+      return {
+        isSubValue : props.isSubValue,
+        open : props.isSubValue
+      };
+    }
+
+    return null;
+  }
+
+  handleClick = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  render() {
+    const { open } = this.state;
+    const { children, summary } = this.props;
+
+    return (
+      <React.Fragment>
+        <Button onClick={this.handleClick}>
+          {open && <ExpandLess />}
+          {!open && <ExpandMore />}
+        </Button>
+
+        <Collapse in={!open} timeout="auto" unmountOnExit>
+          {summary}
+        </Collapse>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          {children}
+        </Collapse>
+      </React.Fragment>
+    );
+  }
+
+  static propTypes = {
+    children : PropTypes.node,
+    summary  : PropTypes.node
+  }
+}
 
 const NullValue = () => (
   <Typography>{'(null)'}</Typography>
@@ -38,80 +90,95 @@ BoolValue.propTypes = {
   value : PropTypes.bool.isRequired
 };
 
-const ArrayValue = ({ value }) => (
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell>Value</TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {value.map((value, index) => (
-        <TableRow key={index}>
-          <TableCell><Value value={value} /></TableCell>
+const ArrayValue = ({ value, isSubValue }) => (
+  <ExtensibleValue isSubValue={isSubValue} summary={
+    <Typography>{JSON.stringify(value)}</Typography>
+  }>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Value</TableCell>
         </TableRow>
-      ))}
-    </TableBody>
-  </Table>
+      </TableHead>
+      <TableBody>
+        {value.map((value, index) => (
+          <TableRow key={index}>
+            <TableCell><Value isSubValue={true} value={value} /></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </ExtensibleValue>
 );
 
 ArrayValue.propTypes = {
-  value : PropTypes.array.isRequired
+  value : PropTypes.array.isRequired,
+  isSubValue : PropTypes.bool
 };
 
-const ObjectValue = ({ value }) => (
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell>Name</TableCell>
-        <TableCell>Value</TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {Object.entries(value).map(([ key, value ]) => (
-        <TableRow key={key}>
-          <TableCell>{key}</TableCell>
-          <TableCell><Value value={value} /></TableCell>
+const ObjectValue = ({ value, isSubValue }) => (
+  <ExtensibleValue isSubValue={isSubValue} summary={
+    <Typography>{JSON.stringify(value)}</Typography>
+  }>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Value</TableCell>
         </TableRow>
-      ))}
-    </TableBody>
-  </Table>
+      </TableHead>
+      <TableBody>
+        {Object.entries(value).map(([ key, value ]) => (
+          <TableRow key={key}>
+            <TableCell>{key}</TableCell>
+            <TableCell><Value isSubValue={true} value={value} /></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </ExtensibleValue>
 );
 
 ObjectValue.propTypes = {
-  value : PropTypes.object.isRequired
+  value : PropTypes.object.isRequired,
+  isSubValue : PropTypes.bool
 };
 
-const TableValue = ({ value }) => {
+const TableValue = ({ value, isSubValue }) => {
   const keys = Array.from(value.reduce((set, row) => {
     Object.keys(row).forEach(key => set.add(key));
     return set;
   }, new Set()));
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          {keys.map(key => (<TableCell key={key}>{key}</TableCell>))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {value.map((row, index) => (
-          <TableRow key={index}>
-            {keys.map(key => (
-              <TableCell key={key}>
-                <Value value={row[key]} />
-              </TableCell>
-            ))}
+    <ExtensibleValue isSubValue={isSubValue} summary={
+      <Typography>{JSON.stringify(value)}</Typography>
+    }>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {keys.map(key => (<TableCell key={key}>{key}</TableCell>))}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {value.map((row, index) => (
+            <TableRow key={index}>
+              {keys.map(key => (
+                <TableCell key={key}>
+                  <Value isSubValue={true} value={row[key]} />
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </ExtensibleValue>
   );
 };
 
 TableValue.propTypes = {
-  value : PropTypes.arrayOf(PropTypes.object.isRequired).isRequired
+  value : PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  isSubValue : PropTypes.bool
 };
 
 const components = {
@@ -124,10 +191,10 @@ const components = {
   table   : TableValue
 };
 
-const Value = ({ value }) => {
+const Value = ({ value, isSubValue = false }) => {
   const type = typeOf(value);
   const Component = components[type];
-  return (<Component value={value} />);
+  return (<Component value={value} isSubValue={isSubValue} />);
 };
 
 Value.propTypes = {
@@ -137,7 +204,8 @@ Value.propTypes = {
     PropTypes.number,
     PropTypes.object,
     PropTypes.string,
-  ])
+  ]),
+  isSubValue : PropTypes.bool
 };
 
 export default Value;
